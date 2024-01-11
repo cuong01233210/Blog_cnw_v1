d8715ef036d3178262047dd5cf481b0c7ed4b17d
cuong <cuong16102002@gmail.com>
2024-01-11 19:54:24 +0700

    xong thêm comment

--- Desktop/blog/src/ShowEveryoneBlog.js
+++ Desktop/blog/src/ShowEveryoneBlog.js
@@ -1,13 +1,14 @@
-// ShowEveryoneBlog.js
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useLocation } from "react-router-dom";
-
+import "./ShowEveryoneBlog.css";
 const ShowEveryoneBlog = () => {
   const location = useLocation();
   const { userEmail } = location.state || {};
   const [blogs, setBlogs] = useState([]);
-  const [fetchedBlogs, setFetchedBlogs] = useState([]);
+  const [activeBlogId, setActiveBlogId] = useState(null);
+  const [comment, setComment] = useState("");
+
   useEffect(() => {
     const fetchBlogs = async () => {
       try {
@@ -16,7 +17,6 @@ const ShowEveryoneBlog = () => {
           return;
         }
 
-        // Check if blogs are already available, if yes, don't make the request again
         if (!blogs || blogs.length === 0) {
           const response = await axios.post(
             "http://localhost:8000/get-blog-except-email",
@@ -32,6 +32,31 @@ const ShowEveryoneBlog = () => {
     fetchBlogs();
   }, [userEmail, blogs]);
 
+  const handleSubmit = async (e, blogEmail) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/add-blog-comment",
+        {
+          blogId: activeBlogId,
+          ownerEmail: blogEmail,
+          commentEmail: userEmail, // Assuming the comment is made by the same user
+          comment: comment,
+        }
+      );
+      if (response.data.message === "success") {
+        alert("Comment added successfully!");
+      } else {
+        alert("Failed to add comment.");
+      }
+    } catch (error) {
+      console.error("Failed to add comment:", error.message);
+      alert("Failed to add comment.");
+    }
+    setActiveBlogId(null);
+    setComment("");
+  };
+
   return (
     <div>
       <h1>Everyone's Blogs</h1>
@@ -46,6 +71,19 @@ const ShowEveryoneBlog = () => {
               <source src={blog.audio} type="audio/mp3" />
               Your browser does not support the audio element.
             </audio>
+            <button onClick={() => setActiveBlogId(blog.id)}>
+              Create Comment
+            </button>
+            {activeBlogId === blog.id && (
+              <form onSubmit={(e) => handleSubmit(e, blog.email)}>
+                <input
+                  type="text"
+                  value={comment}
+                  onChange={(e) => setComment(e.target.value)}
+                />
+                <button type="submit">Submit</button>
+              </form>
+            )}
           </div>
         ))}
       </div>
